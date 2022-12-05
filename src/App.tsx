9ccc06ee1fb25fdb4f337cbed64d607c4c7f95c5
Ahmad <ahmadjarafat@gmail.com>
2022-12-05 21:34:25 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { Object3D, Vector3, Mesh } from "three";
+import { useRef, useEffect, useState } from "react";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import {
+  useGLTF,
+  SpotLight,
+  useDepthBuffer,
+  OrbitControls,
+} from "@react-three/drei";
 
 function App() {
+  const [windowDimensions, setWindowDimensions] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    setWindowDimensions({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+
+    function handleResize() {
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+      setWindowDimensions({ width: width, height: height });
+    }
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas
+      style={{ width: windowDimensions.width, height: windowDimensions.height }}
+      camera={{ position: [-2, 4, 9], fov: 50, near: 1, far: 20 }}
+    >
+      <color attach="background" args={["#202020"]} />
+      <ambientLight intensity={0.5} />
+      <Scene />
+    </Canvas>
+  );
+}
+
+function Scene() {
+  const cube = useRef<Mesh>(null!);
+
+  useFrame((delta) => {
+    cube.current.rotateX(0.01);
+    cube.current.rotateY(0.01);
+  });
+  return (
+    <>
+      <MovingSpot color="#0c8cbf" position={[3, 3, 2]} />
+      <MovingSpot color="#b00c3f" position={[1, 3, 0]} />
+      <mesh position={[0, 0.8, 0]} ref={cube}>
+        <boxGeometry args={[2, 2, 2]} />
+        <meshPhysicalMaterial
+          transmission={1}
+          roughness={0}
+          thickness={0.1}
+          color={"#87CEEB"}
+          specularIntensity={0.4}
+          ior={2.2}
+          clearcoat={0.5}
+        />
+      </mesh>
+      <mesh receiveShadow position={[0, -1, 0]} rotation-x={-Math.PI / 2}>
+        <planeGeometry args={[50, 50]} />
+        <meshPhongMaterial color={"red"} />
+      </mesh>
+      <mesh position={[0, 0, -5]}>
+        <planeGeometry args={[50, 50]} />
+        <meshPhongMaterial />
+      </mesh>
+    </>
+  );
+}
+
+function MovingSpot({ ...props }) {
+  const light = useRef<Object3D>(null!);
+
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime();
+    const target = light.current.children[0];
+    target.position.setX(Math.sin(time) * 2);
+  });
+  return (
+    <object3D ref={light}>
+      <SpotLight
+        castShadow
+        penumbra={1}
+        distance={6}
+        angle={0.35}
+        attenuation={5}
+        anglePower={4}
+        intensity={2}
+        {...props}
+      />
+    </object3D>
   );
 }
 
